e702d766bac9605929052ab59118673bbdc852ae
Punya <punya.juneja@senpiper.com>
2025-06-24 14:52:18 +0530

    gradle plugin version updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform") version "1.9.23" apply false
     kotlin("plugin.serialization") version "1.9.23" apply false
-    id("com.android.library") version "8.3.0" apply false
+    id("com.android.library") version "8.8.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("org.jetbrains.compose") version "1.6.10" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.23" apply false
